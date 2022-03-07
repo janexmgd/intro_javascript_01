97668c86cd00547bf36b52b9735d0d83a3995099
unknown <janexmgd>
2022-03-07 18:09:35 +0700

    kedua

--- nomor2.js
+++ nomor2.js
@@ -1,27 +1,34 @@
 //nomor2
-const mtk = 100
-const bahasaIndonesia = 150
-const bahasaInggris = 100
-const ipa = null
+const mtk = 85
+const bahasaIndonesia = 50
+const bahasaInggris = 60
+const ipa = 75
+
 //rata rata nilai un
 let average = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
-// validasi input number semua bukan
+// validasi semua input number atau bukan
 if (typeof (mtk) == "number" && typeof (bahasaIndonesia) == "number" && typeof (bahasaInggris) == "number" && typeof (ipa) == "number") {
-    console.log(`rata rata nilainya adalah ${average}`);
-    // memberi grade grade
-    if (average >= 100) {
-        console.log("Maaf rata-rata nilai tak boleh diatas 100")
-    } else if (average <= 100 && average >= 90) {
-        console.log("Grade A");
-    } else if (average <= 89 && average >= 80) {
-        console.log("Grade B")
-    } else if (average <= 79 && average >= 70) {
-        console.log("Grade C")
-    } else if (average <= 69 && average >= 60) {
-        console.log("Grade D")
+    // validasi nilai input lebih dari 100 atau tidak
+    if (mtk <= 100 && bahasaIndonesia <= 100 && bahasaInggris <= 100 && ipa <= 100) {
+        if (average <= 100 && average >= 90) {
+            console.log(`rata rata nilainya adalah ${average}`);
+            console.log("Grade A");
+        } else if (average <= 89 && average >= 80) {
+            console.log(`rata rata nilainya adalah ${average}`);
+            console.log("Grade B")
+        } else if (average <= 79 && average >= 70) {
+            console.log(`rata rata nilainya adalah ${average}`);
+            console.log("Grade C")
+        } else if (average <= 69 && average >= 60) {
+            console.log(`rata rata nilainya adalah ${average}`);
+            console.log("Grade D")
+        } else {
+            console.log(`rata rata nilainya adalah ${average}`);
+            console.log("Grade E")
+        }
     } else {
-        console.log("Grade E")
+        console.log(`input nilai tak boleh lebih dari 100`)
     }
 } else {
-    console.log(`Tolong input data type number`)
+    console.log(`mohon input data number`);
 }
